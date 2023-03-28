72645feaa3a17de054ab54cae7731c117b36007d
Samuel Andert <samuel@ora.earth>
2023-03-28 11:29:10 +0200

    update flowbite install

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
+
 <script>
 	import { Alert } from 'flowbite-svelte';
 </script>
+
+<h1 class="text-3xl dark:text-white">Welcome to SvelteKit</h1>
+<p class="dark:text-white">
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
 
 <div class="p-8">
 	<Alert>
