91abe7ea46abc6cf1d068e8c75d955687439efe8
Samuel Andert <samuel@ora.earth>
2023-03-28 11:55:21 +0200

    Added basic navigation bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,28 @@
 <script>
 	import '../app.css';
 	import { DarkMode } from 'flowbite-svelte';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger,Skeleton, ImagePlaceholder, Tooltip,  } from 'flowbite-svelte'
 	let darkmodebtn =
 		'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-lg p-2.5 fixed right-4 top-2 z-50';
 </script>
 
-<DarkMode btnClass={darkmodebtn} />
-<slot />
+<Navbar let:hidden let:toggle>
+	<NavBrand href="/">
+	  <img
+		src="https://flowbite.com/docs/images/logo.svg"
+		class="mr-3 h-6 sm:h-9"
+		alt="Flowbite Logo"
+	  />
+	  <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+		Ora Earth
+	  </span>
+	</NavBrand>
+	<NavHamburger on:click={toggle} />
+	<NavUl {hidden}>
+	  <NavLi href="/" active={true}>Home</NavLi>
+	  <NavLi href="/about">About</NavLi>
+	</NavUl>
+	<DarkMode btnClass={darkmodebtn} />
+  </Navbar>
+
+  <slot />
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+
+<script>
+	import { Alert } from 'flowbite-svelte';
+</script>
+
+<h1 class="text-3xl dark:text-white">Welcome to About</h1>
+
+
