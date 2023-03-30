7498c4c9c4a854787cc1505ab262e79d2ba2f52e
Samuel Andert <samuel@ora.earth>
2023-03-30 18:23:11 +0200

    added todos to apps

--- src/routes/apps/[id]/+page.svelte
+++ src/routes/apps/[id]/+page.svelte
@@ -9,7 +9,6 @@
 		Heading,
 		Secondary
 	} from 'flowbite-svelte';
-	let spanClass = 'flex-1 ml-3 whitespace-nowrap';
 
 	export let data;
 
@@ -54,9 +53,19 @@
 				>{$AppById.data.apps_by_id.name}<br />
 				<Secondary class="ml-2">{$AppById.data.apps_by_id.id}</Secondary></Heading
 			>
-			<List tag="ul" class="space-y-1">
+			<List tag="ul" class="divide-y divide-gray-200 dark:divide-gray-700">
 				{#each $AppById.data.apps_by_id.todos as todo}
-					<Li>{todo.todos_id.name}</Li>
+					<Li class="py-2 sm:py-2" icon>
+						<div class="flex items-center space-x-2">
+							<div class="flex-shrink-0" />
+							<div class="flex-1 min-w-0">
+								<p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+									{todo.todos_id.name}
+								</p>
+								<p class="text-sm text-gray-500 truncate dark:text-gray-400">{todo.todos_id.id}</p>
+							</div>
+						</div>
+					</Li>
 				{/each}
 			</List>
 		</div>
