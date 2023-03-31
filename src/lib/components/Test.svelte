2b2a99b1bd22927ba24c78072a2e5763fc885a9d
Samuel Andert <samuel@ora.earth>
2023-03-31 14:17:55 +0200

    dynamically loading a svelte test component

---
+++ src/lib/components/Test.svelte
@@ -0,0 +1,1 @@
+<div class="text-yellow-700 dark:text-yellow-500">This is local</div>
--- src/routes/apps/[id]/+page.svelte
+++ src/routes/apps/[id]/+page.svelte
@@ -1,14 +1,11 @@
 <script>
-	import {
-		Li,
-		List,
-		Sidebar,
-		SidebarGroup,
-		SidebarItem,
-		SidebarWrapper,
-		Heading,
-		Secondary
-	} from 'flowbite-svelte';
+	import { Li, List, Heading, Secondary } from 'flowbite-svelte';
+
+	let Test;
+
+	function loadComponent() {
+		import('$lib/components/Test.svelte').then((res) => (Test = res.default));
+	}
 
 	export let data;
 
@@ -20,33 +17,13 @@
 {:else}
 	<div class="grid grid-cols-12">
 		<div class="col-span-3">
-			<Sidebar>
-				<SidebarWrapper>
-					<SidebarGroup>
-						<SidebarItem label="List Of Views">
-							<svelte:fragment slot="icon">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="w-6 h-6"
-									><path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z"
-									/><path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z"
-									/></svg
-								>
-							</svelte:fragment>
-						</SidebarItem>
-					</SidebarGroup>
-				</SidebarWrapper>
-			</Sidebar>
+			<!-- <Test /> -->
+			<button on:click={loadComponent}>Load Component</button>
+			<svelte:component this={Test} />
+
+			<!-- {#each $AppById.data.apps_by_id.components as component}
+				<svelte:component this={option.component} />
+			{/each} -->
 		</div>
 		<div class="col-span-9">
 			<Heading tag="h4"
