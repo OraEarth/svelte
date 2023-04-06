0f47456f80c740d63ac8c7436895fbf5b3bf17f7
Samuel Andert <samuel@ora.earth>
2023-04-06 14:31:19 +0200

    cleanup unfinished session implementation

--- src/client.js
+++ src/client.js
@@ -1,14 +1,5 @@
 import { HoudiniClient } from '$houdini';
 
-console.log("session: " + session?.user)
-
 export default new HoudiniClient({
     url: 'https://data.ora.earth/graphql',
-    fetchParams({ session }) {
-        return {
-            headers: {
-                Authorization: `Bearer ${session?.user.token}`,
-            },
-        }
-    },
 })
